c713b4f93772a544196d598d4f602ec7d6959b46
coding-donjake <108913937+coding-donjake@users.noreply.github.com>
2023-05-17 15:25:37 +0800

    update

--- version-2/src/pages/DashboardPage.tsx
+++ version-2/src/pages/DashboardPage.tsx
@@ -6,9 +6,24 @@ function DashboardPage() {
     <>
       <SideNavigation />
       <div className="offset">
-        <h1>Panel Name</h1>
-        <div className="block">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae dolore vero, aperiam asperiores consequatur quisquam libero ad officia dolores saepe neque cumque nulla consequuntur facilis. Voluptatibus alias nemo perspiciatis enim!
+        <h1>Dashboard</h1>
+        <div className="blocks-wrapper">
+          <div className="block" style={{ backgroundColor: 'rgb(249 115 22)' }}>
+            <h2>Upcoming Events</h2>
+            <span>10</span>
+          </div>
+          <div className="block" style={{ backgroundColor: 'rgb(14 165 233)' }}>
+            <h2>Current Events</h2>
+            <span>2</span>
+          </div>
+          <div className="block" style={{ backgroundColor: 'rgb(34 197 94)' }}>
+            <h2>Catered Events</h2>
+            <span>20</span>
+          </div>
+          <div className="block" style={{ backgroundColor: 'rgb(239 68 68)' }}>
+            <h2>Canceled Events</h2>
+            <span>20</span>
+          </div>
         </div>
       </div>
     </>
